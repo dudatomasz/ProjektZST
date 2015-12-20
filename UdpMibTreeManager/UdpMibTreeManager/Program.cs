e397ddbb48d6db03a8d94345a7e7258717f3c86f
BartlomiejOstrowski <ostry28@gmail.com>
2015-12-20 15:57:13 +0100

    zmiana

--- UdpMibTreeManager/UdpMibTreeManager/Program.cs
+++ UdpMibTreeManager/UdpMibTreeManager/Program.cs
@@ -1,11 +1,11 @@
-﻿using System;
+﻿using Lextm.SharpSnmpLib;
+using Lextm.SharpSnmpLib.Messaging;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using Lextm.SharpSnmpLib;
-using Lextm.SharpSnmpLib.Messaging;
-using System.Net;
 
 namespace UdpMibTreeManager
 {
@@ -17,23 +17,9 @@ namespace UdpMibTreeManager
         [STAThread]
         static void Main()
         {
-            
-            /*Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);         
-            Application.Run(new Form1());*/
-            var result = Messenger.Get(VersionCode.V1,
-                            new IPEndPoint(IPAddress.Parse("192.168.0.11"), 161),
-                            new OctetString("ProjektZST"),
-                            new List<Variable> { new Variable(new ObjectIdentifier("1.3.6.1.2.1.1.1.0")) },
-                            60000);
-            Console.WriteLine(result[0].ToString());
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
-            Console.WriteLine();
-
-
-
         }
     }
 }
