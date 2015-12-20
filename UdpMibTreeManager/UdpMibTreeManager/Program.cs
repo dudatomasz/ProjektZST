190db14f30967418691a403bab0f91b8359b0fd5
Tomasz Duda <dudatomasz@outlook.com>
2015-12-20 12:09:24 +0100

    komentarz

--- UdpMibTreeManager/UdpMibTreeManager/Program.cs
+++ UdpMibTreeManager/UdpMibTreeManager/Program.cs
@@ -19,6 +19,7 @@ namespace UdpMibTreeManager
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
             // tghjkl;
+            // TOMEKTOMEKTOMEK
         }
     }
 }
